94a59e8298068842209f27ccdb407bd14e8ae5b8
lukwagoasuman <lukwagoasuman236@gmail.com>
2024-09-05 17:03:35 +0300

    Darude --Sandstorm

--- models/Stock.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const stockSchema = new mongoose.Schema({
-  stock: { type: String, required: true, unique: true },
-  likes: { type: [String], default: [] } // Store hashed IPs
-});
-
-module.exports = mongoose.model('Stock', stockSchema);
